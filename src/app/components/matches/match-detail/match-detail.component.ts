f03b18df4bb9ee2efcefd33982cfa0b29c30122d
Keval <kevalvadhiya101@gmail.com>
2024-06-26 17:39:52 +0530

    format document

--- src/app/components/matches/match-detail/match-detail.component.ts
+++ src/app/components/matches/match-detail/match-detail.component.ts
@@ -8,10 +8,9 @@ import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-match-detail',
   templateUrl: './match-detail.component.html',
-  styleUrls: ['./match-detail.component.scss']
+  styleUrls: ['./match-detail.component.scss'],
 })
 export class MatchDetailComponent {
-
   players$ = new BehaviorSubject([]);
 
   matchId: string;
@@ -29,8 +28,8 @@ export class MatchDetailComponent {
   bowlerForm: FormGroup;
   tossForm: FormGroup;
   matchRunsDetail = {
-    team1: {runs: 0, wickets: 0},
-    team2: {runs: 0, wickets: 0},
+    team1: { runs: 0, wickets: 0 },
+    team2: { runs: 0, wickets: 0 },
   };
 
   constructor(
@@ -38,24 +37,23 @@ export class MatchDetailComponent {
     private fb: FormBuilder,
     private router: Router,
     private messageService: MessageService,
-    private route: ActivatedRoute) {
-    
-      this.strikerForm = this.fb.group({
-        striker: '',
-        nonStriker: '',
-      })
+    private route: ActivatedRoute
+  ) {
+    this.strikerForm = this.fb.group({
+      striker: '',
+      nonStriker: '',
+    });
 
-      this.bowlerForm = this.fb.group({
-        bowler: '',
-      })
+    this.bowlerForm = this.fb.group({
+      bowler: '',
+    });
 
-      this.tossForm = this.fb.group({
-        winTheToss: '',
-        selected: '',
-      })
+    this.tossForm = this.fb.group({
+      winTheToss: '',
+      selected: '',
+    });
   }
 
-  
   ngOnInit() {
     this.matchId = this.route.snapshot.params['matchId'];
     this.getMatchDetail();
@@ -66,8 +64,8 @@ export class MatchDetailComponent {
   }
 
   saveStriker() {
-    const payload = {...this.match, ...this.strikerForm.value}
-    this.dataService.updateMatch(payload)
+    const payload = { ...this.match, ...this.strikerForm.value };
+    this.dataService.updateMatch(payload);
     this.selectStriker = false;
   }
 
@@ -76,8 +74,8 @@ export class MatchDetailComponent {
   }
 
   saveBowler() {
-    const payload = {...this.match, ...this.bowlerForm.value}
-    this.dataService.updateMatch(payload)
+    const payload = { ...this.match, ...this.bowlerForm.value };
+    this.dataService.updateMatch(payload);
     this.selectBowler = false;
   }
 
@@ -86,9 +84,9 @@ export class MatchDetailComponent {
   }
 
   saveTheToss() {
-    const payload = {...this.match, toss: {...this.tossForm.value}}
+    const payload = { ...this.match, toss: { ...this.tossForm.value } };
     payload['isStarted'] = true;
-    this.dataService.updateMatch(payload)
+    this.dataService.updateMatch(payload);
     this.getMatchDetail();
     this.toss = false;
   }
@@ -99,184 +97,229 @@ export class MatchDetailComponent {
   }
 
   getMatchDetail() {
-    this.dataService.getMatch(this.matchId).subscribe(match => {
+    this.dataService.getMatch(this.matchId).subscribe((match) => {
       this.match = match;
 
-      if(this.match.bowler) {
-        const balls = this.match.bowler.over > 0 ? this.match.bowler.over.split('.')?.[1] : 0;
-        if(balls) {
+      if (this.match.bowler) {
+        const balls =
+          this.match.bowler.over > 0
+            ? this.match.bowler.over.split('.')?.[1]
+            : 0;
+        if (balls) {
           this.currentBall = +balls;
         }
       }
 
-      if(this.match.toss) {
+      if (this.match.toss) {
         const winningTossTeam = this.match.toss.winTheToss;
-        const lossingTossTeam = this.match.toss.winTheToss === 'team1' ? 'team2' : 'team1';
-        this.battingTeam = this.match.toss.selected == 'Bat First' ? winningTossTeam : lossingTossTeam;
-        this.bowlingTeam = this.match.toss.selected == 'Bowl First' ? winningTossTeam : lossingTossTeam;
+        const lossingTossTeam =
+          this.match.toss.winTheToss === 'team1' ? 'team2' : 'team1';
+        this.battingTeam =
+          this.match.toss.selected == 'Bat First'
+            ? winningTossTeam
+            : lossingTossTeam;
+        this.bowlingTeam =
+          this.match.toss.selected == 'Bowl First'
+            ? winningTossTeam
+            : lossingTossTeam;
       }
 
       this.setTeamScores();
-    })
+    });
   }
 
   getPlayers() {
-    this.dataService.getPlayers().subscribe(players => {
-      this.players$.next(players)
-    })
+    this.dataService.getPlayers().subscribe((players) => {
+      this.players$.next(players);
+    });
   }
 
   setTeamScores() {
-    let team1 = {runs: 0, wickets: 0};
-    let team2 = {runs: 0, wickets: 0};
-    this.match.team1.players.map(x => {
-      if(x.matches.length) {
-        const matchIndex = x.matches.findIndex(x => x.matchId === this.matchId);
-        team1.runs =  x.matches[matchIndex]?.runs ? x.matches[matchIndex].runs + team1.runs : team1.runs;
-        team2.wickets =  x.matches[matchIndex]?.wickets ? x.matches[matchIndex].wickets + team2.wickets : team2.wickets;
+    let team1 = { runs: 0, wickets: 0 };
+    let team2 = { runs: 0, wickets: 0 };
+    this.match.team1.players.map((x) => {
+      if (x.matches.length) {
+        const matchIndex = x.matches.findIndex(
+          (x) => x.matchId === this.matchId
+        );
+        team1.runs = x.matches[matchIndex]?.runs
+          ? x.matches[matchIndex].runs + team1.runs
+          : team1.runs;
+        team2.wickets = x.matches[matchIndex]?.wickets
+          ? x.matches[matchIndex].wickets + team2.wickets
+          : team2.wickets;
       }
-    })
+    });
 
-    this.match.team2.players.map(x => {
-      if(x.matches.length) {
-        const matchIndex = x.matches.findIndex(x => x.matchId === this.matchId);
-        team2.runs =  x.matches[matchIndex]?.runs ? x.matches[matchIndex].runs + team2.runs : team2.runs;
-        team1.wickets =  x.matches[matchIndex]?.wickets ? x.matches[matchIndex].wickets + team1.wickets : team1.wickets;
+    this.match.team2.players.map((x) => {
+      if (x.matches.length) {
+        const matchIndex = x.matches.findIndex(
+          (x) => x.matchId === this.matchId
+        );
+        team2.runs = x.matches[matchIndex]?.runs
+          ? x.matches[matchIndex].runs + team2.runs
+          : team2.runs;
+        team1.wickets = x.matches[matchIndex]?.wickets
+          ? x.matches[matchIndex].wickets + team1.wickets
+          : team1.wickets;
       }
-    })
+    });
 
     this.matchRunsDetail = {
       team1: team1,
       team2: team2,
-    }
-
+    };
   }
-
-
 
   setBowling(score) {
     const striker = this.match.striker;
     const nonStriker = JSON.parse(JSON.stringify(this.match.nonStriker));
-    const strikerIndex = this.match[this.battingTeam].players.findIndex(x => x.uid === striker.uid)
-    const nonStrikerIndex = this.match[this.battingTeam].players.findIndex(x => x.uid === nonStriker.uid)
+    const strikerIndex = this.match[this.battingTeam].players.findIndex(
+      (x) => x.uid === striker.uid
+    );
+    const nonStrikerIndex = this.match[this.battingTeam].players.findIndex(
+      (x) => x.uid === nonStriker.uid
+    );
     const bowler = this.match.bowler;
-    const bowlerIndex = this.match[this.bowlingTeam].players.findIndex(x => x.uid === bowler.uid);
+    const bowlerIndex = this.match[this.bowlingTeam].players.findIndex(
+      (x) => x.uid === bowler.uid
+    );
 
     let strikerRuns = {};
     // handle bowlers runs
     this.maidenBallCount = score === 0 ? this.maidenBallCount + 1 : 0;
-      const strikerMatch = this.match[this.battingTeam].players[strikerIndex]?.matches?.filter(x => x.matchId === this.match.id) ?? [];
-      const strikerDetail = strikerMatch.length ? strikerMatch[0] : {};
-      strikerRuns = {
-        ...strikerDetail,
-        matchId: this.match.id,
-        runs: score != 'OUT' ? strikerDetail.runs > 0 ? strikerDetail.runs + +score : score : strikerDetail.runs, 
-        balls: strikerDetail.balls > 0 ? strikerDetail.balls + 1 : 1,
-        out: score === 'OUT'
-      }
-  
-      if(score === 6) {
-        strikerRuns['sixes'] = strikerDetail.sixes > 0 ? strikerDetail.sixes + 1 : 1;
-      } else if(score === 4) {
-        strikerRuns['fours'] = strikerDetail.fours > 0 ? strikerDetail.fours + 1 : 1;
-      }
-  
-      const matchIndex = this.match[this.battingTeam].players[strikerIndex]?.matches?.findIndex(x => x.matchId === this.match.id);
-  
-      if(matchIndex != -1 && matchIndex >= 0) {
-        this.match[this.battingTeam].players[strikerIndex].matches[matchIndex] = strikerRuns;
-      } else {
-        this.match[this.battingTeam].players[strikerIndex].matches.push(strikerRuns);
-      }
+    const strikerMatch =
+      this.match[this.battingTeam].players[strikerIndex]?.matches?.filter(
+        (x) => x.matchId === this.match.id
+      ) ?? [];
+    const strikerDetail = strikerMatch.length ? strikerMatch[0] : {};
+    strikerRuns = {
+      ...strikerDetail,
+      matchId: this.match.id,
+      runs:
+        score != 'OUT'
+          ? strikerDetail.runs > 0
+            ? strikerDetail.runs + +score
+            : score
+          : strikerDetail.runs,
+      balls: strikerDetail.balls > 0 ? strikerDetail.balls + 1 : 1,
+      out: score === 'OUT',
+    };
 
+    if (score === 6) {
+      strikerRuns['sixes'] =
+        strikerDetail.sixes > 0 ? strikerDetail.sixes + 1 : 1;
+    } else if (score === 4) {
+      strikerRuns['fours'] =
+        strikerDetail.fours > 0 ? strikerDetail.fours + 1 : 1;
+    }
 
-   // handle bowlers runs/balls
-   this.currentBall = this.currentBall + 1
+    const matchIndex = this.match[this.battingTeam].players[
+      strikerIndex
+    ]?.matches?.findIndex((x) => x.matchId === this.match.id);
 
-   const bowlerMatch = this.match[this.bowlingTeam].players[bowlerIndex]?.matches?.filter(x => x.matchId === this.match.id) ?? [];
-   const bowlerDetail = bowlerMatch.length ? bowlerMatch[0] : {};
+    if (matchIndex != -1 && matchIndex >= 0) {
+      this.match[this.battingTeam].players[strikerIndex].matches[matchIndex] =
+        strikerRuns;
+    } else {
+      this.match[this.battingTeam].players[strikerIndex].matches.push(
+        strikerRuns
+      );
+    }
 
-   const bowlerRuns = {
-    ...bowlerDetail,
-    matchId: this.match.id,
-    balls: bowlerDetail.balls > 0 ? bowlerDetail.balls + 1 : 1,
-  }
+    // handle bowlers runs/balls
+    this.currentBall = this.currentBall + 1;
 
-   if(score === 'OUT') {
-      bowler['wickets'] = bowler?.wicket ? +bowler.wicket + 1 : 1; 
+    const bowlerMatch =
+      this.match[this.bowlingTeam].players[bowlerIndex]?.matches?.filter(
+        (x) => x.matchId === this.match.id
+      ) ?? [];
+    const bowlerDetail = bowlerMatch.length ? bowlerMatch[0] : {};
+
+    const bowlerRuns = {
+      ...bowlerDetail,
+      matchId: this.match.id,
+      balls: bowlerDetail.balls > 0 ? bowlerDetail.balls + 1 : 1,
+    };
+
+    if (score === 'OUT') {
+      bowler['wickets'] = bowler?.wicket ? +bowler.wicket + 1 : 1;
       bowlerRuns.wickets = bowler.wickets;
     } else {
-      bowler['concededRuns'] = bowler?.concededRuns > 0 ? bowler.concededRuns + +score : score;
+      bowler['concededRuns'] =
+        bowler?.concededRuns > 0 ? bowler.concededRuns + +score : score;
       bowlerRuns.concededRuns = bowler.concededRuns;
     }
 
-    if(this.currentBall === 6) {
-      const over = this.match.bowler?.over?.split('.')?.[0] ?? 0
+    if (this.currentBall === 6) {
+      const over = this.match.bowler?.over?.split('.')?.[0] ?? 0;
       bowler['over'] = bowler.over > 0 ? `${+over + 1}.0` : '1.0';
-      bowlerRuns.over =  bowler.over;
-      if(this.maidenBallCount === 6) {
+      bowlerRuns.over = bowler.over;
+      if (this.maidenBallCount === 6) {
         bowler['maidens'] = bowler?.maidens > 0 ? bowler.maidens + 1 : 1;
         bowlerRuns.maidens = bowler.maidens;
       }
-
     } else {
-      const over = this.match.bowler?.over?.split('.')?.[0] ?? 0
-      bowler['over'] = bowler.over > 0 ? `${over}.${this.currentBall}` : `0.${this.currentBall}`;
-      bowlerRuns.over =  bowler.over;
+      const over = this.match.bowler?.over?.split('.')?.[0] ?? 0;
+      bowler['over'] =
+        bowler.over > 0
+          ? `${over}.${this.currentBall}`
+          : `0.${this.currentBall}`;
+      bowlerRuns.over = bowler.over;
     }
 
-    const matchBwIndex = this.match[this.bowlingTeam].players[bowlerIndex]?.matches?.findIndex(x => x.matchId === this.match.id);
+    const matchBwIndex = this.match[this.bowlingTeam].players[
+      bowlerIndex
+    ]?.matches?.findIndex((x) => x.matchId === this.match.id);
 
-    if(matchBwIndex != -1 && matchBwIndex >= 0) {
-      this.match[this.bowlingTeam].players[bowlerIndex].matches[matchBwIndex] = bowlerRuns
+    if (matchBwIndex != -1 && matchBwIndex >= 0) {
+      this.match[this.bowlingTeam].players[bowlerIndex].matches[matchBwIndex] =
+        bowlerRuns;
     } else {
-      this.match[this.bowlingTeam].players[bowlerIndex].matches.push(bowlerRuns);
+      this.match[this.bowlingTeam].players[bowlerIndex].matches.push(
+        bowlerRuns
+      );
     }
-    
 
-    this.match.striker = {...this.match.striker, ...strikerRuns};
-    this.match.bowler = {...this.match.bowler, ...bowlerRuns};
+    this.match.striker = { ...this.match.striker, ...strikerRuns };
+    this.match.bowler = { ...this.match.bowler, ...bowlerRuns };
 
-    // this.match.team1.players[strikerIndex].runs = this.match.team1.players[strikerIndex].runs > 0 ? this.match.team1.players[strikerIndex].runs + +score : score;
-    // this.match.team1.players[strikerIndex].balls = this.match.team1.players[strikerIndex].balls > 0 ? this.match.team1.players[strikerIndex].balls + 1 : 1
-    // if(score === 6) {
-    //   this.match.team1.players[strikerIndex].sixes = this.match.team1.players[strikerIndex].sixes > 0 ? this.match.team1.players[strikerIndex].sixes + 1 : 1;
-    // } else if(score === 4) {
-    //   this.match.team1.players[strikerIndex].fours = this.match.team1.players[strikerIndex].fours > 0 ? this.match.team1.players[strikerIndex].fours + 1 : 1;
-    // }
-
-    if(score === 1 || this.currentBall === 6 && score != 'OUT') {
+    if (score === 1 || (this.currentBall === 6 && score != 'OUT')) {
       this.match.nonStriker = this.match.striker;
       this.match.striker = nonStriker;
-    } else if(score === 'OUT') {
-      this.match['outBatsman'] = this.match?.outBatsman ? [...this.match.outBatsman, this.match.striker.id] : [this.match.striker.id]
+    } else if (score === 'OUT') {
+      this.match['outBatsman'] = this.match?.outBatsman
+        ? [...this.match.outBatsman, this.match.striker.id]
+        : [this.match.striker.id];
       this.match.striker = {};
     }
 
     this.currentBall = this.currentBall === 6 ? 0 : this.currentBall;
 
-    this.match['team1Score'] = this.match['team1Score'] > 0 ? +this.match['team1Score'] + 1 : 0;
-    const payload = {...this.match, bowler}
+    this.match['team1Score'] =
+      this.match['team1Score'] > 0 ? +this.match['team1Score'] + 1 : 0;
+    const payload = { ...this.match, bowler };
 
     const batsmanPlayer = this.match[this.battingTeam].players[strikerIndex];
     const bowlingPlayer = this.match[this.bowlingTeam].players[bowlerIndex];
 
-    this.dataService.updatePlayer(batsmanPlayer)
-    this.dataService.updatePlayer(bowlingPlayer)
-    this.dataService.updateTeam(this.match[this.battingTeam])
-    this.dataService.updateTeam(this.match[this.bowlingTeam])
+    this.dataService.updatePlayer(batsmanPlayer);
+    this.dataService.updatePlayer(bowlingPlayer);
+    this.dataService.updateTeam(this.match[this.battingTeam]);
+    this.dataService.updateTeam(this.match[this.bowlingTeam]);
 
     this.selectRuns = false;
     this.setTeamScores();
-    this.dataService.updateMatch(payload)
+    this.dataService.updateMatch(payload);
   }
 
-
-
-
   calculateStrikeRate(totalRuns, totalBallsFaced) {
-    if (totalBallsFaced === 0) { return 0;}
-    return !isNaN((totalRuns / totalBallsFaced) * 100) ? ((totalRuns / totalBallsFaced) * 100).toFixed(2) : 0;
+    if (totalBallsFaced === 0) {
+      return 0;
+    }
+    return !isNaN((totalRuns / totalBallsFaced) * 100)
+      ? ((totalRuns / totalBallsFaced) * 100).toFixed(2)
+      : 0;
   }
 
   calculateEconomy(overs, runs) {
@@ -289,8 +332,12 @@ export class MatchDetailComponent {
   async deleteMatch() {
     if (confirm('Are you sure to delete match?') == true) {
       await this.dataService.deleteMatch(this.matchId);
-      this.messageService.add({ severity: 'success', summary: 'Match', detail: 'Deleted Successfully!' });
-      this.router.navigate(['/matches'])
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Match',
+        detail: 'Deleted Successfully!',
+      });
+      this.router.navigate(['/matches']);
     }
   }
 }
