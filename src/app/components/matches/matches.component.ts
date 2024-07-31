d9a275bbb63f5b4ebb61946cee5b69b34e88537b
Keval <kevalvadhiya101@gmail.com>
2024-07-31 17:46:08 +0530

    Updates

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -11,16 +11,31 @@ import { DataService } from 'src/app/services/data.service';
 export class MatchesComponent {
   matches$ = new BehaviorSubject([])
   visible: boolean;
+  userId: string;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.userId = localStorage.getItem('userId');
+
     this.getMatches();
   }
 
   getMatches() {
-    this.dataService.getMatches().subscribe(teams => {
-      this.matches$.next(teams)
+    this.dataService.getMatches().subscribe(matches => {
+      const data = [];
+      matches.map(x => {
+        let player;
+        if(x.team1.players.find(p => p.id === this.userId)) {
+          player = x.team1.players.find(p => p.id === this.userId)
+        } else if(x.team2.players.find(p => p.id === this.userId)) {
+          player = x.team2.players.find(p => p.id === this.userId)
+        }
+        if(player) {
+          data.push(x);
+        }
+      })
+      this.matches$.next(data)
     })
   }
 
