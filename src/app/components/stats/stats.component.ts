ff05e54b9f2166767beccee919900ac670b3624c
Keval Vadhiya <kevalvadhiya101@gmail.com>
2025-04-04 09:33:23 +0530

    fix

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -251,5 +251,21 @@ export class StatsComponent {
         return `${overs}.${remainingBalls}`;
     }
 }
+
+addNumberSuffix(num) {
+  const j = num % 10;
+  const k = num % 100;
+
+  if (j === 1 && k !== 11) {
+    return num + "st";
+  }
+  if (j === 2 && k !== 12) {
+    return num + "nd";
+  }
+  if (j === 3 && k !== 13) {
+    return num + "rd";
+  }
+  return num + "th";
+}
   
 }
