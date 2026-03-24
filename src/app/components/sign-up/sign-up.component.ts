f5fd22c8feae3b1831a1a2ab7214a2978713de78
user <user@gmail.com>
2026-03-24 15:22:54 +0530

    Fix Signup

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -23,10 +23,10 @@ export class SignUpComponent {
     this.signupForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', Validators.required],
-      bat: ['', Validators.required],
-      bowl: ['', Validators.required],
+      bat: [''],
+      bowl: [''],
       password: ['', Validators.required],
-      type: ['', Validators.required],
+      type: [''],
       matches: [[]]
     })
   }
@@ -37,22 +37,22 @@ export class SignUpComponent {
         email: this.signupForm.value.email,
         password: this.signupForm.value.password
       }
-      this.authService.signup(payload.email, payload.password).then(res => {
-        const user = res.user.multiFactor.user;
-        const data = this.signupForm.value;
-        data['uid'] = user.uid
-        this.authService.storeUserData(user.uid, data)
-        this.messageService.add({ severity: 'success', summary: 'Register', detail: 'Register Successfully!' });
-        this.router.navigate(['/dashboard']);
-      }).catch(error => {
-        if(error.message.includes('weak-password')) {
-          this.messageService.add({ severity: 'error', summary: 'Week Password', detail: 'Password should be at least 6 characters' });
-        } else if(error.message.includes('invalid-email')) {
-          this.messageService.add({ severity: 'error', summary: 'Invalid Email', detail: 'The email address is Invalid' });
-        }  else if(error.message.includes('email-already-in-use')) {
-          this.messageService.add({ severity: 'error', summary: 'Email Already In Use', detail: 'The email address is already in use by another account' });
-        }
-      })
+      // this.authService.signup(payload.email, payload.password).then(res => {
+      //   const user = res.user.multiFactor.user;
+      //   const data = this.signupForm.value;
+      //   data['uid'] = user.uid
+      //   this.authService.storeUserData(user.uid, data)
+      //   this.messageService.add({ severity: 'success', summary: 'Register', detail: 'Register Successfully!' });
+      //   this.router.navigate(['/dashboard']);
+      // }).catch(error => {
+      //   if(error.message.includes('weak-password')) {
+      //     this.messageService.add({ severity: 'error', summary: 'Week Password', detail: 'Password should be at least 6 characters' });
+      //   } else if(error.message.includes('invalid-email')) {
+      //     this.messageService.add({ severity: 'error', summary: 'Invalid Email', detail: 'The email address is Invalid' });
+      //   }  else if(error.message.includes('email-already-in-use')) {
+      //     this.messageService.add({ severity: 'error', summary: 'Email Already In Use', detail: 'The email address is already in use by another account' });
+      //   }
+      // })
     }
   }
 }
